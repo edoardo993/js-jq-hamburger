af3e148d99072490549d5716e4aeffbf1d845121
Edoardo Piragine <edoardo.piragine@gmail.com>
2021-01-12 17:19:38 +0100

    aggiunte altre varianti

--- js/main.js
+++ js/main.js
@@ -10,3 +10,29 @@ $('.header-right a').click(function() {
 $('.close').click(function() {
  $('.hamburger-menu').hide();
 });
+
+// creo due funzioni, una che mi mostra
+// il menu 'nascosto' cliccando
+// sull'hamburger menu facendolo apparire
+// con un fadeIn di 1 sec, l'altra
+// che me lo fa scomparire quando si
+// clicca sulla X con un fadeOut di 1 sec
+
+// $('.header-right a').click(function() {
+//  $('.hamburger-menu').fadeIn(1000);
+// });
+// $('.close').click(function() {
+//  $('.hamburger-menu').fadeOut(1000);
+// });
+
+// creo due funzioni, una che mi mostra
+// il menu 'nascosto' passando col mouse
+// sull'hamburger menu, l'altra
+// che me lo fa scomparire quando si
+// clicca sulla X
+// $('.header-right a').mouseenter(function() {
+//  $('.hamburger-menu').show();
+// });
+// $('.close').click(function() {
+//  $('.hamburger-menu').hide();
+// });
